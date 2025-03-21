601916fded5f79148e8ba39c65b2abbd36830563
josee022 <jose.mondelo@iesdonana.org>
2025-03-21 18:42:11 +0100

    page_size: 10

--- techhub_backend/settings.py
+++ techhub_backend/settings.py
@@ -144,6 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',  # Habilitar DRF Spectacular
+    'PAGE_SIZE': 10,  # Número de dispositivos por página
 }
 
 SIMPLE_JWT = {
